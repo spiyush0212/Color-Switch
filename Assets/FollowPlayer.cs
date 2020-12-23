4fd42d07eb5b8062522c2b2781333f5dc3705624
Piyush Sharma <spiyush.0212@gmail.com>
2020-12-24 04:31:32 +0530

    Final working game

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( player.position.y > transform.position.y )
+        {
+            transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        }
+    }
+}
